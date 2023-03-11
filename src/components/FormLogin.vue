427ebd8a0ebaba0706a86f7df3c532e0670aa2ca
Henrique Zuffo <henriquezuffo@gmail.com>
2023-03-11 20:05:25 -0300

    First commit

---
+++ src/components/FormLogin.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-form ref="form" class="ma-10">
+    <v-text-field class="field" type="input" label="usuario" 
+      hint="Digite o nome de usuário" append-inner-icon="mdi-account-outline"
+      v-model="user"
+    />
+
+    <v-text-field class="field" type="password" label="senha" 
+      hint="Digite o sua senha" append-inner-icon="mdi-eye-off-outline"
+      v-model="passwd"
+    />
+
+    <v-btn /> 
+
+  </v-form>
+  
+</template>
+
+<script lang="ts">
+  export default{
+    data(){
+      return{
+        user: '',
+        passwd: ''
+      }
+    }
+  }
+</script>
+  
+
+<style>
+
+</style>
---
+++ src/layouts/default/Default.vue
@@ -0,0 +1,22 @@
+<template>
+    <main>
+        <router-view />
+    </main>
+    
+</template>
+
+
+<script lang="ts" setup>
+
+
+</script>
+
+<style>
+    body{
+        background-image: url("../../assets/bg.png");
+        background-color: #cccc;
+        background-repeat: no-repeat;
+        background-size: cover;        
+    }
+  
+</style>
